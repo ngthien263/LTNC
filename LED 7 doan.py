7746f25c850898ad6ce12e12cef2b3658d51fac7
NgThienn <109409996+NgThienn@users.noreply.github.com>
2024-03-03 21:55:22 +0700

    Update LED 7 doan.py

--- LED 7 doan.py
+++ LED 7 doan.py
@@ -1,13 +1,20 @@
 import math
 import numpy as np
+import RPi.GPIO as GPIO
 import time
-import numpy as np
 def dec2bin(x):
     x = list(map(int,bin(x)[2:]))
-    y = list(np.zeros(7 - np.size(x),int))
-    y.extend(x)
-    return y
+    a = list(map(int, np.zeros(7 - len(x))))
+    a.extend(x)
+    return a
+GPIO.setmode(GPIO.BOARD)
 port = [7, 11, 12, 13, 15, 16, 18]
 num_list = [126,48,109,51,91,95,112,127,123]
-y =  dec2bin(48)
-print(np.size(y))
+for i in range (len(port)):
+    GPIO.setup(port[i], GPIO.OUT)
+while(1):
+   num =  num_list[2]
+   y = dec2bin(num)
+   for i in range (len(y)):
+       GPIO.output(port[i], y[i])
+       time.sleep(0.1)
